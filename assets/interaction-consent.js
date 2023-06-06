dea5a1fefcbd9bc6958b29e8bc7468e839f5fc03
metastable-void <git@yuka.h09.eu>
2023-06-06 19:09:19 +0900

    safari comat

--- assets/interaction-consent.js
+++ assets/interaction-consent.js
@@ -1,5 +1,8 @@
 
+const consentForm = document.querySelector('#consent_form');
 const button = document.querySelector('button[type="submit"]');
-button.addEventListener('click', () => {
+button.addEventListener('click', (ev) => {
+  ev.preventDefault();
   button.disabled = true;
+  consentForm.submit();
 });
